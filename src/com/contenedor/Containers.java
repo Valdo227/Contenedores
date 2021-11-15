16e8663d0f89565b020ded4c3065286a727ccd33
HiramOrd <hiram8012@gmail.com>
2021-11-14 21:12:17 -0500

    feat (containers) ui config added

--- src/com/contenedor/Containers.java
+++ src/com/contenedor/Containers.java
@@ -1,4 +1,17 @@
 package com.contenedor;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Containers {
+    private JPanel panel1;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Contenedores");
+        frame.setContentPane(new Containers().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setMinimumSize(new Dimension(480, 480));
+        frame.setVisible(true);
+    }
 }
